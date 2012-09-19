e03eacb41f8c6e918e5c4b96cfdbf28f6d61f26e
enriquecruzmachado@gmail.com <enriquecruzmachado@gmail.com@7866635b-d149-46c6-a564-df0b3c7e5da5>
2012-09-19 23:09:58 +0000

    mapa de telas atualizado

---
+++ trunk/BD Script/FunctionsAlt2.sql
@@ -0,0 +1,13 @@
+﻿CREATE OR REPLACE FUNCTION inserir_tipoconsulta(valorconsulta numeric, desc_tipo character varying)
+  RETURNS integer AS
+$BODY$
+	declare
+		cod integer;
+	begin
+		insert into tipo_consulta(valor_consulta, descricao_tipo_cconsulta) VALUES (valorconsulta, desc_tipo);
+		GET DIAGNOSTICS cod = RESULT_OID;
+
+		return cod;
+	end;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
