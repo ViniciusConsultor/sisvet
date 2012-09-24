a6b17acf39d296e94570c792f1da8a2257ccdf31
phyllipe.bm <phyllipe.bm@gmail.com>
2012-09-24 23:59:37 +0000

    Inclusão das functions.sql para atualização do sistema

---
+++ BD Script/functions delete trigger.sql
@@ -0,0 +1,12 @@
+﻿create or replace function pagar_conta(codcusto integer) returns varchar AS
+$BODY$
+
+begin 
+	delete from custos where cod_cliente_fk = codcusto;
+
+	return "Efetuado baixa no sistema";
+end;
+
+$BODY$
+language plpgsql
+
---
+++ BD Script/functions laboratoriais.sql
@@ -0,0 +1,17 @@
+﻿create or replace function inserir_laboratoriais(tratamento integer, exame integer, descricao text,
+responsavel character varying, valor numeric)
+returns integer as 
+$BODY$
+
+declare 
+	cod integer;
+begin
+	insert into laboratoriais(cod_tratamento_fk, cod_exame_fk, descricao_exame, responsavel_exame,
+	valor) values (tratamento, exame, descricao, responsavel, valor);
+
+	GET DIAGNOSTICS cod = RESULT_OID;
+	return cod;
+end;
+
+$BODY$
+language plpgsql;
